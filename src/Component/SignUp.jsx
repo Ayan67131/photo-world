bdd30d3d78b6927dbe21349c0a1cb4df3180f7ac
Corejava02 <Rickdas8777@gmail.com>
2023-11-15 09:53:21 +0530

    Register API

--- src/Component/SignUp.jsx
+++ src/Component/SignUp.jsx
@@ -1,6 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './SignUp.css'
+import Axios from 'axios'
 const SignUp = () => {
+  const url = "http://localhost:8080/user/add";
+  const[data,setData] = useState({
+    name:"",
+    email:"",
+    address:"",
+    phone:""
+  })
+
+  function submit(e){
+    e.preventDefault();
+    Axios.post(url,{
+      name:data.name,
+      email:data.email,
+      address:data.address,
+      phone:data.phone
+    })
+    .then(res=>{
+      console.log(res.data)
+    })
+  }
+
+  function handle(e){
+    const newData={...data}
+    newData[e.target.id] = e.target.value
+    setData(newData)
+    console.log(newData)
+  }
   return (
     <div class="SignUp">
     <h1>Sign Up</h1>
@@ -9,39 +37,49 @@ const SignUp = () => {
       <div class="card-body">
         {/* form  */}
 
-        <form>
+        <form onSubmit={(e)=>submit(e)}>
           <div class="form-group">
             <label for="exampleInputName">Name</label>
             <input
-              type="name"
+              type="text"
               class="form-control"
               id="name"
               aria-describedby="nameHelp"
               placeholder="Name"
+              onChange={(e)=>handle(e)}
+              value={data.name}
             />
             <label for="exampleInputPhone">Phone Number</label>
             <input
-              type="phone"
+              type="number"
               class="form-control"
-              id="exampleInputPhone"
+              id="phone"
               aria-describedby="phoneHelp"
               placeholder="Phone"
+              onChange={(e)=>handle(e)}
+              value={data.phone}
             />
             <label for="exampleInputEmail1">Email address</label>
             <input
               type="email"
               class="form-control"
-              id="exampleInputEmail1"
+              id="email"
               aria-describedby="emailHelp"
               placeholder="Email"
+              onChange={(e)=>handle(e)}
+              value={data.email}
             />
             <label for="exampleInputAddress">Address</label>
             <input
-              type="address"
+              type="textarea"
+              row="4"
+              cols="50"
               class="form-control"
-              id="exampleInputAddress"
+              id="address"
               aria-describedby="emailHelp"
               placeholder="Address"
+              onChange={(e)=>handle(e)}
+              value={data.address}
             />
             <small id="emailHelp" class="form-text text-muted">
               We'll never share your email with anyone else.
@@ -52,9 +90,6 @@ const SignUp = () => {
           <button type="submit" class="btn btn-primary btn-block">
             Sign Up
           </button>
-
-          
-
         </form>
       </div>
     </div>
